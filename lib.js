bc6a7e1bd57009d9bf5b630d19e3e15a32faa353
Brian Yee <brian.a.yee@gmail.com>
2013-12-07 23:22:36 -0800

    Make the stuff actually display.
    This is an ugly first swing at things.
    
    You return the string in JavaScript but don’t actually set it in HTML
    (the DOM). Use the innerHTML property to set that.
    
    Also, run the randomWifiName() function when the page loads.

--- lib.js
+++ lib.js
@@ -1,8 +1,12 @@
 var wifiNames = ['Wi Believe I Can Fi',
                  'Hide Yo Kids Hide Yo WiFi',
                  'Pineapple'];
+
 // Returns a random wifi name string
 function randomWifiName() {
-  var ret = wifiNames[Math.floor(Math.random() * wifiNames.length)];
-  return ret;
+  document.getElementById('name').innerHTML =  wifiNames[Math.floor(Math.random() * wifiNames.length)];
+}
+
+window.onload = function() {
+  randomWifiName();
 }
